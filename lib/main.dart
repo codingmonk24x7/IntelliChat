801868bb811ca8c334f0f7247f7f89753a250384
Ramansh Agarwal <ramanshag001@gmail.com>
2023-06-04 06:23:40 +0530

    Added Screens and routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 import 'screens/splash_screen.dart';
+import 'screens/login_screen.dart';
+import 'screens/signup_screen.dart';
+import 'screens/chat_screen.dart';
 
 void main() {
   runApp(
     MaterialApp(
       initialRoute: 'splash',
       routes: {
-        'splash' : (context) => SplashScreen()
+        'splash' : (context) => SplashScreen(),
+        'login' : (context) => LoginScreen(),
+        'signup' : (context) => SignupScreen(),
+        'chat' : (context) => ChatScreen(),
       },
     )
   );
